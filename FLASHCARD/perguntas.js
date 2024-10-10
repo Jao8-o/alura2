628ea81849c4998025cffbf4f38dc74652826407
Jao8-o <joaovgromero@gmail.com>
2024-10-10 07:43:29 -0300

    Add files via upload

---
+++ FLASHCARD/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'DISNEY',
+    'QUAL E O NOME DA MAE3 DA PRINCESA TIANA?',
+    'EUDORA'
+)
+
+criaCartao(
+    'NARUTO',
+    'QUAL E O NOME VERDADEIRO DO YAMATO?',
+    'TENZO'
+)
+
+criaCartao(
+    'CURIOSIDADE',
+    'EM QUE ANO FOI CRIADO O CHUVEIRO ELETRICO?',
+    '1945'
+)
+
+criaCartao(
+    'FATOS',
+    'QUEM MATOU O MICHAEL JACKSON?',
+    'p.DIDDYYYYY'
+)
